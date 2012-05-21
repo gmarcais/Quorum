634f9c8377f352a4c037285bc1935eee7f4aa550
Guillaume Marcais <Guillaume Marcais guillaume@marcais.net>
2012-05-21 11:29:41 -0400

    Minor changes to yaggo's output

--- src/error_correct_reads_cmdline.hpp
+++ src/error_correct_reads_cmdline.hpp
@@ -159,12 +159,12 @@ public:
         break;
       case 'c':
         combined_given = true;
-        combined_arg = yaggo::conv_uint<uint32_t>((const char *)optarg, err, false);
+        combined_arg = yaggo::conv_uint<uint32_t>((const char*)optarg, err, false);
         CHECK_ERR(uint32_t, optarg, "-c, --combined=Nb-DB")
         break;
       case 't':
         thread_given = true;
-        thread_arg = yaggo::conv_uint<uint32_t>((const char *)optarg, err, false);
+        thread_arg = yaggo::conv_uint<uint32_t>((const char*)optarg, err, false);
         CHECK_ERR(uint32_t, optarg, "-t, --thread=uint32")
         break;
       case 'C':
@@ -172,32 +172,32 @@ public:
         break;
       case 'm':
         min_count_given = true;
-        min_count_arg = yaggo::conv_uint<uint32_t>((const char *)optarg, err, false);
+        min_count_arg = yaggo::conv_uint<uint32_t>((const char*)optarg, err, false);
         CHECK_ERR(uint32_t, optarg, "-m, --min-count=uint32")
         break;
       case 's':
         skip_given = true;
-        skip_arg = yaggo::conv_uint<uint32_t>((const char *)optarg, err, false);
+        skip_arg = yaggo::conv_uint<uint32_t>((const char*)optarg, err, false);
         CHECK_ERR(uint32_t, optarg, "-s, --skip=uint32")
         break;
       case 'g':
         good_given = true;
-        good_arg = yaggo::conv_uint<uint32_t>((const char *)optarg, err, false);
+        good_arg = yaggo::conv_uint<uint32_t>((const char*)optarg, err, false);
         CHECK_ERR(uint32_t, optarg, "-g, --good=uint32")
         break;
       case 'a':
         anchor_count_given = true;
-        anchor_count_arg = yaggo::conv_uint<uint32_t>((const char *)optarg, err, false);
+        anchor_count_arg = yaggo::conv_uint<uint32_t>((const char*)optarg, err, false);
         CHECK_ERR(uint32_t, optarg, "-a, --anchor-count=uint32")
         break;
       case 'w':
         window_given = true;
-        window_arg = yaggo::conv_uint<uint32_t>((const char *)optarg, err, false);
+        window_arg = yaggo::conv_uint<uint32_t>((const char*)optarg, err, false);
         CHECK_ERR(uint32_t, optarg, "-w, --window=uint32")
         break;
       case 'e':
         error_given = true;
-        error_arg = yaggo::conv_uint<uint32_t>((const char *)optarg, err, false);
+        error_arg = yaggo::conv_uint<uint32_t>((const char*)optarg, err, false);
         CHECK_ERR(uint32_t, optarg, "-e, --error=uint32")
         break;
       case 'o':
@@ -213,7 +213,7 @@ public:
         break;
       case HOMO_TRIM_OPT:
         homo_trim_given = true;
-        homo_trim_arg = yaggo::conv_int<int32_t>((const char *)optarg, err, false);
+        homo_trim_arg = yaggo::conv_int<int32_t>((const char*)optarg, err, false);
         CHECK_ERR(int32_t, optarg, "    --homo-trim=int32")
         break;
       case GZIP_OPT:
@@ -294,5 +294,4 @@ public:
   }
 private:
 };
-
 #endif // __ARGS_T_HPP__"

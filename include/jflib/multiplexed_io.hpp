945ff94775ac7cb458fdda043c86a618ba9b3b08
Guillaume Marçais <gmarcais@umd.edu>
2014-05-29 15:35:27 -0400

    Fix bug in multiplexed_io.hpp. Copied from SuperReads.

--- include/jflib/multiplexed_io.hpp
+++ include/jflib/multiplexed_io.hpp
@@ -1,29 +1,60 @@
+/* SuperRead pipeline
+ * Copyright (C) 2012  Genome group at University of Maryland.
+ * 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #ifndef _JFLIB_MULTIPLEXED_IO_H_
 #define _JFLIB_MULTIPLEXED_IO_H_
 
 #include <cstdlib>
 #include <cstring>
 #include <cstdio>
+#include <cstddef>
 #include <ostream>
 #include <stdexcept>
 #include <jflib/pool.hpp>
 
-/** Multiplexed io stream. Multiple threads write to a pool of buffers
- * which are then written by a single thread to an ostream. Similar to
- * asynchronous IO, potentially easier to use.
- *
- * This works in 2 steps: first create the o_multiplexer based on an
- * std::ostream. This will setup the pool. Then, each thread creates
- * an omstream based on the o_multiplexer. Each of this omstream
- * obtain and send buffers to the ostream using the pool. To ensure
- * that the output is not interleaved with other threads output, there
- * is a end_record() (or << endr) method which marks the end of a
- * record that can be send to the underlying ostream and written as
- * one atomic operation. A sync() operation (or << std::flush) implies
- * an end of record as well.
- */
-
 namespace jflib {
+  /** Output multiplexer class. Multiple threads write to a pool of
+   * buffers which are then written by a single thread to an
+   * ostream. Similar to asynchronous IO, potentially easier to use.
+   *
+   * This works in 2 steps: first create the o_multiplexer based on an
+   * `std::ostream`. This will setup the pool/FIFO. Then, each thread creates
+   * an `omstream` based on the `o_multiplexer`. Each of this `omstream`
+   * obtain and send buffers to the ostream using the pool. To ensure
+   * that the output is not interleaved with other threads output,
+   * there is a `end_record()` (or `<< endr`) method which marks the
+   * end of a record that can be send to the underlying ostream and
+   * written as one atomic operation. A `sync()` operation (or `<<
+   * std::flush`) implies an end of record as well.
+   *
+   * An example:
+   * ~~~{.cc}
+   * std::ofstream file("/path/to/file"); // Open file for output
+   * jflib::o_multiplexer multiplexer(file); // Create multiplexer to file
+   *
+   * // In each thread
+   * jflib::omstream out(multiplexer); // Create my multiplexed ostream
+   * out << "Hello world!\n" << "How are you doing?\n" << endr;
+   * ~~~
+   *
+   * The two lines are guaranteed to not be interleaved with output
+   * from other thread. They are an atomic record.
+   */
   class o_multiplexer {
     class                buffer;
     class                multiplexedbuf;
@@ -34,15 +65,18 @@ namespace jflib {
       writer_info(std::ostream *_os, buffer_pool *_pool) : os(_os), pool(_pool) { }
     };
   public:
-    /** Constructor based on an ostream os and nb_buffers of size
-     * buffer_size. For reliable performance, there should be more
-     * nb_buffers than threads that will create omstream, otherwise
-     * there will always be threads waiting on getting a
+    /** Constructor based on an ostream `os` and `nb_buffers` of size
+     * `buffer_size`.
+     * @param os Underlying stream to write to.
+     *
+     * @param nb_buffers For reliable performance, there should be
+     * more nb_buffers than threads that will create omstream,
+     * otherwise there will always be threads waiting on getting a
      * buffer. Probably, 2 to 3 times the number of threads.
      *
-     * The size of a buffer should be larger than the record
-     * length. If not, the buffer get enlarged (size doubled) when a
-     * record does not fit into a buffer.
+     * @param buffer_size The size of a buffer should be larger than
+     * the record length. If not, the buffer get enlarged (size
+     * doubled) when a record does not fit into a buffer.
      */
     o_multiplexer(std::ostream *os, size_t nb_buffers, size_t buffer_size)
       : _os(os), _pool(nb_buffers), _info(_os, &_pool), _writer_started(false)
@@ -63,13 +97,14 @@ namespace jflib {
      * destructor.
      */
     virtual ~o_multiplexer() { close(); }
+
     /** Tell the writer thread to stop after the pool is empty. To be
      * safe, this should be called after every threads with an
      * omstream() associated have are done writing to the
      * stream. Unspecified behavior if a thread writes after close has
      * been called.
      *
-     * TODO: should we implement a safe version that waits for every
+     * @todo should we implement a safe version that waits for every
      * omstream object to be destroyed?
      */
     void close() {
@@ -114,17 +149,20 @@ namespace jflib {
       void set_write_len(std::streamsize s) { _write_len = s; }
       bool do_sync() const { return _sync; }
       void set_do_sync(bool sync) { _sync = sync; }
-      char *resize(size_t size = 0) {
-        size_t csize = _end - _data;
-        if(!size)
-          size = 2 * csize;
-        if(size <= csize)
+      size_t size() const { return _end - _data; }
+      char *resize(size_t min_size = 0) {
+        const size_t csize = size();
+        if(!min_size)
+          min_size = 2 * csize;
+        if(min_size <= csize)
           return _data;
-        char *ndata = (char*)realloc(_data, size);
+        if(min_size == 0)
+          min_size = 4096;
+        char *ndata = (char*)realloc(_data, min_size);
         if(!ndata)
           return 0;
         _data = ndata;
-        _end  = _data + size;
+        _end  = _data + min_size;
         return _data;
       }
     private:
@@ -180,7 +218,7 @@ namespace jflib {
         size_t clen = pptr() - pbase();
         if(clen == 0)
           return;
-        _elt->set_write_len(pptr() - pbase());
+        _elt->set_write_len(clen);
         ++_nbr;
         if(2 * clen >= (size_t)(epptr() - pptr()) * _nbr) {
           _elt->set_do_sync(false);
@@ -191,17 +229,19 @@ namespace jflib {
       virtual int overflow(int c) {
         if(_closed)
           return EOF;
+
         if(_nbr == 0) { // No record in its entirety
-          size_t old_size = _elt->end() - _elt->base();
+          ptrdiff_t old_size = _elt->end() - _elt->base();
           if(!_elt->resize())
             return EOF;
           setp(_elt->base(), _elt->end());
           pbump(old_size);
         } else { // Save part not in a record and release rest.
-          std::ptrdiff_t in_buf = pptr() - pbase();
+          ptrdiff_t in_buf = pptr() - pbase();
           if(_elt->write_len() < in_buf) {
             size_t extra = in_buf - _elt->write_len();
-            _overflow_buffer.resize(extra);
+            if(!_overflow_buffer.resize(extra))
+              return EOF;
             memcpy(_overflow_buffer.base(), pptr() - extra, extra);
             _overflow_buffer.set_write_len(extra);
           } else
@@ -211,8 +251,11 @@ namespace jflib {
           if(!update_buffer())
             return EOF;
           if(_overflow_buffer.write_len() > 0) {
-            memcpy(_elt->base(), _overflow_buffer.base(),
-                   _overflow_buffer.write_len());
+            const char* nbase = _elt->resize(2 * _overflow_buffer.write_len());
+            if(!nbase)
+              return EOF;
+            setp(_elt->base(), _elt->end());
+            memcpy(pbase(), _overflow_buffer.base(), _overflow_buffer.write_len());
             pbump(_overflow_buffer.write_len());
           }
         }
@@ -230,7 +273,7 @@ namespace jflib {
       bool update_buffer() {
         if(!_closed)
           _closed = _pool->is_closed_B_to_A();
-
+        
         if(_closed) {
           _elt.release();
           setp(0, 0);
@@ -260,6 +303,8 @@ namespace jflib {
     bool          _writer_started;
   };
 
+  /** Multiplexed ostream. 
+   */
   class omstream : public std::ostream {
   public:
     omstream(o_multiplexer &om) : std::ostream(om.new_multiplexedbuf()) { }
@@ -267,9 +312,9 @@ namespace jflib {
       rdbuf()->pubsync();
       delete rdbuf();
     }
-    /** Mark the end of a record
-     */
+    /** Mark the end of a record */
     void end_record() { ((o_multiplexer::basebuf*)rdbuf())->end_record(); }
+    /** Close this ostream */
     void close() { 
       rdbuf()->pubsync();
       delete rdbuf(new o_multiplexer::closedbuf());
